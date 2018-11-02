f582af46378adc0c3317c1e33b17c7d027072efc
Zach Southwell <Zach@SouthwellHome.com>
2018-11-02 07:17:41 -0500

    Add logic to calculate 'Equity' of a matching

--- src/EquitableMatcher.java
+++ src/EquitableMatcher.java
@@ -22,6 +22,34 @@ public class EquitableMatcher {
 		List<List<Person>> manOptimalMatch = GaleShapelyAlgorithm.execute(cloneGroups(groups), "m");
 		List<List<Person>> womanOptimalMatch = GaleShapelyAlgorithm.execute(cloneGroups(groups), "w");
 
+		Long equityScore = calculateEquityScore(manOptimalMatch);
+		System.out.println("Man-optimal equity score: " + equityScore);
+		equityScore = calculateEquityScore(womanOptimalMatch);
+		System.out.println("Woman-optimal equity score: " + equityScore);
+	}
+
+
+	private Long calculateEquityScore(List<List<Person>> manOptimalMatch) {
+		List<Person> men = manOptimalMatch.get(0);
+		List<Person> women = manOptimalMatch.get(1);
+		Long equity = 0L;
+		equity = men.stream() //
+				.map(man -> calculatePreferenceScore(man, women))
+				.reduce(0L, (a, b) -> a + b);
+		equity += women.stream() //
+				.map(woman -> calculatePreferenceScore(woman, men))
+				.reduce(0L, (a, b) -> a + b);
+		return equity;
+	}
+
+
+	private Long calculatePreferenceScore(Person person, List<Person> preferenceGroup) {
+		Integer matchIndex = preferenceGroup.indexOf(person.getMatch());
+		if (matchIndex < 0) {
+			throw new RuntimeException("invalid match");
+		}
+		Integer preferenceListIndex = person.getPreferenceList().indexOf(matchIndex);
+		return preferenceListIndex.longValue();
 	}
 
 
