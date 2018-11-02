8c28ed8f8ce2e7940d443004a7086226c296bb32
Zachry Southwell <zach@southwellhome.com>
2018-11-02 16:13:16 -0500

    Completed recursive method to find optimal matching...

--- src/EquitableMatcher.java
+++ src/EquitableMatcher.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.SerializationUtils;
 
 public class EquitableMatcher {
+
+	private Long bestMatchValue = Long.MAX_VALUE;
+	private List<List<Person>> bestMatch = null;
+	private Integer numberOfMatchings = 0;
 
 	public static void main(String[] args) {
 		if (args.length != 1) {
@@ -21,34 +26,104 @@ public class EquitableMatcher {
 		List<List<Person>> manOptimalMatch = GaleShapelyAlgorithm.execute(cloneGroups(groups), "m");
 		List<List<Person>> womanOptimalMatch = GaleShapelyAlgorithm.execute(cloneGroups(groups), "w");
 
-		trimFeasiblePreferences(groups, manOptimalMatch, womanOptimalMatch);
-
-
 		Long equityScore = calculateEquityScore(manOptimalMatch);
 		System.out.println("Man-optimal equity score: " + equityScore);
 		equityScore = calculateEquityScore(womanOptimalMatch);
 		System.out.println("Woman-optimal equity score: " + equityScore);
+
+		trimAllFeasiblePreferences(groups, manOptimalMatch, womanOptimalMatch);
+
+		findMostEquitableMatch(groups);
+
+		equityScore = calculateEquityScore(bestMatch);
+		System.out.println("Total matchings: " + numberOfMatchings + " Optimal equity score: " + equityScore);
+
+	}
+
+	private void findMostEquitableMatch(List<List<Person>> input) {
+		bestMatchValue = Long.MAX_VALUE;
+		bestMatch = null;
+		numberOfMatchings = 0;
+		findMostEquitableMatch(input.get(0).size(), input);
 	}
 
 
-	private void trimFeasiblePreferences(List<List<Person>> groups, List<List<Person>> manOptimalMatch,
+	private void findMostEquitableMatch(Integer unmatchedCount, List<List<Person>> input) {
+		//base case: unmatched list is empty:
+		if (unmatchedCount == 0) {
+			numberOfMatchings++;
+			Long equityScore = calculateEquityScore(input);
+			if (equityScore < bestMatchValue) {
+				bestMatchValue = equityScore;
+				bestMatch = input;
+			}
+			return;
+		}
+
+		//recursive case:
+		List<List<Person>> traversal = cloneGroups(input);
+		List<Person> availableMen = traversal.get(0);
+		List<Person> availableWomen = traversal.get(1);
+		Person availableMan = availableMen.get(availableMen.size() - unmatchedCount);
+		List<Integer> feasibleMatches = availableMan.getFeasiblePreferences();
+		for (Integer feasibleMatchIndex : feasibleMatches) {
+			Person feasibleWoman = availableWomen.get(feasibleMatchIndex);
+			//is this match still in the list of available options?
+			if (feasibleWoman.getMatch() != null) {
+				continue;
+			}
+			availableMan.setMatch(feasibleWoman);
+			feasibleWoman.setMatch(availableMan);
+			findMostEquitableMatch(unmatchedCount - 1, cloneGroups(traversal));
+			availableMan.setMatch(null);
+			feasibleWoman.setMatch(null);
+		}
+
+	}
+
+
+	private void trimAllFeasiblePreferences(List<List<Person>> groups, List<List<Person>> manOptimalMatch,
 			List<List<Person>> womanOptimalMatch) {
 		List<Person> men = groups.get(0);
-		List<Person> women = groups.get(0);
+		List<Person> women = groups.get(1);
 		List<Person> menOptimal = manOptimalMatch.get(0);
 		List<Person> menPessimal = womanOptimalMatch.get(0);
 		List<Person> womenOptimal = womanOptimalMatch.get(1);
 		List<Person> womenPessimal = manOptimalMatch.get(1);
 
-		trimFeasiblePreferences(men, women, menOptimal, menPessimal);
-		trimFeasiblePreferences(women, men, womenOptimal, womenPessimal);
+		trimFeasiblePreferencesByGender(men, women, menOptimal, menPessimal);
+		trimFeasiblePreferencesByGender(women, men, womenOptimal, womenPessimal);
 	}
 
-	private void trimFeasiblePreferences(List<Person> men, List<Person> women, List<Person> menOptimal,
-			List<Person> menPessimal) {
-		// TODO Auto-generated method stub
-		
+	private void trimFeasiblePreferencesByGender(List<Person> groupA, List<Person> groupB, List<Person> aOptimal,
+			List<Person> aPessimal) {
+		for (int personIndex = 0; personIndex < groupA.size(); personIndex++) {
+			Person person = groupA.get(personIndex);
+			Person optimalMatch = aOptimal.get(personIndex).getMatch();
+			Person pessimalMatch = aPessimal.get(personIndex).getMatch();
+			Integer optimalMatchIndex = groupB.indexOf(optimalMatch);
+			Integer pessimalMatchIndex = groupB.indexOf(pessimalMatch);
+			trimFeasiblePreferencesForIndividual(person, optimalMatchIndex, pessimalMatchIndex);
+//			System.out.println("person: " + personIndex + " pref: " + person.getPreferenceList() + " feas: " + person.getFeasiblePreferences());
+		}
 	}
+
+	private void trimFeasiblePreferencesForIndividual(Person person, Integer optimalMatchIndex,
+			Integer pessimalMatchIndex) {
+		boolean feasibleRange = false;
+		for (Integer preference : person.getPreferenceList()) {
+			if (preference.equals(optimalMatchIndex)) {
+				feasibleRange = true;
+			}
+			if (!feasibleRange) {
+				person.markInfeasible(preference);
+			}
+			if (preference.equals(pessimalMatchIndex)) {
+				feasibleRange = false;
+			}
+		}
+	}
+
 
 	private Long calculateEquityScore(List<List<Person>> manOptimalMatch) {
 		List<Person> men = manOptimalMatch.get(0);
